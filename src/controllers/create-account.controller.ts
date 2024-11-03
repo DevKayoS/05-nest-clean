c2f0c8ae4579dfbc8ae7dc7f0e51218085f5d58d
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-11-03 01:48:44 -0300

    feat: add hash password

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,5 +1,6 @@
 import { ConflictException } from "@nestjs/common";
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { hash } from "bcryptjs";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Controller('/accounts')
@@ -10,11 +11,12 @@ export class CreateAccountController {
     @Post()
     @HttpCode(201)
     async handle(@Body() body: any){
+        console.log(body)
 
         const {name, email, password} = body
 
         // validando se ja existe usuario
-        const userWithSameEmail = this.prisma.user.findUnique({
+        const userWithSameEmail = await this.prisma.user.findUnique({
             where: {
                 email
             }
@@ -22,15 +24,17 @@ export class CreateAccountController {
 
         if(userWithSameEmail) {
             // status code 409
-            throw new ConflictException('Usuario ja existe')
+            throw new ConflictException('User with same e-mail address already exists')
         }
+
+        // criptografando a senha
+        const hashedPassword = await hash(password, 8)
 
         await this.prisma.user.create({
             data : {
             name,
             email, 
-            password
-        
+            password: hashedPassword
             }
          }   
         )
