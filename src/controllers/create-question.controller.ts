eb89fc2f3ebb54a055af9df1ef1b89d07b281da4
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-11-05 00:35:51 -0300

    feat: tentando finalizar o ocntroller de criacao de perguntas

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,18 +1,55 @@
-import {  Controller, Post, UseGuards } from "@nestjs/common";
+import {  Body, Controller, HttpCode, HttpStatus, Post, UseGuards, UsePipes } from "@nestjs/common";
 import { CurrentUser } from "src/auth/current-user-decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { UserPayload } from "src/auth/jwt.strategy";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
 
+
+const createQuestionBodySchema = z.object({
+    title: z.string(),
+    content: z.string()
+}) 
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard) // fazendo com que a rota precise de autentificacao para ser chamada
 export class CreateQuestionController {
-    constructor(){}
-
+    constructor(private prisma: PrismaService){}
+    
     @Post()
-    async handle(@CurrentUser() user: UserPayload){
-        console.log(user.sub)
+    @HttpCode(201)
+    @UsePipes(new ZodValidationPipe(createQuestionBodySchema)) 
+    async handle(
+        @Body() body: CreateQuestionBodySchema,
+        @CurrentUser() user: UserPayload)
+        {
+        const { title, content } = body
 
-        return 'ok'
+
+        console.log('oi')
+        console.log(title, content)
+
+        
+        await this.prisma.question.create({
+            data: {
+                title,
+                content,
+                slug: this.convertToSlug(title),
+                authorId: user.sub
+            }
+        })
+    }
+
+    private convertToSlug(title: string): string {
+        return title
+        .toLocaleLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .replace(/[^\w\s-]/g, ""
+        .replace(/\s+/g, "-")
+        )
     }
 }
