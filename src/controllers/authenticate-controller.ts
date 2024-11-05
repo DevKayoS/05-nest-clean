b632520f4b6f7f0b99df71caf74d02e4ff80a60d
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-11-04 22:46:59 -0300

    feat: finalizando o controller de autentificacao do usuario

--- src/controllers/authenticate-controller.ts
+++ src/controllers/authenticate-controller.ts
@@ -1,29 +1,51 @@
-import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, HttpCode, NotFoundException, Post, UnauthorizedException, UsePipes } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { compare } from "bcryptjs";
+import { verify } from "crypto";
+import { NotFoundError } from "rxjs";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
 
 
-// const createAccountBodySchema = z.object({
-//     name: z.string(),
-//     email: z.string().email(),
-//     password: z.string()
-// }) 
+const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string()
+}) 
 
-// type CreateACcountBodySchema = z.infer<typeof createAccountBodySchema>
+type AuthenticateAccountBodySchema = z.infer<typeof authenticateBodySchema>
 
 @Controller('/sessions')
 export class AuthenticateConroller {
     //injecao de dependencia
-    constructor(private jwt: JwtService){}
+    constructor(private jwt: JwtService, private prisma: PrismaService){}
 
     @Post()
-    //@HttpCode(201)
+    @HttpCode(200)
     //esse pipe parecido com middleware esta validando os dados e gerando a execption
-    //@UsePipes(new ZodValidationPipe(createAccountBodySchema))
-    async handle(){
-        const token = this.jwt.sign({ sub: 'user-id' })
+    @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+    async handle(@Body() body: AuthenticateAccountBodySchema){
+        const { email, password } = body
+        
+        const user = await this.prisma.user.findUnique({where: { email}})
 
-        return token
+        if(!user){
+            throw new UnauthorizedException('User credentials do not match')
+        }
+
+        const isPasswordValid = await compare(password, user.password)
+
+        if(!isPasswordValid){
+            throw new UnauthorizedException('User Credentials do not match')
+        }
+
+        
+        const accessToken = this.jwt.sign({ sub: user.id })
+
+
+        return {
+            access_token: accessToken
+        }
         
     }
 }
